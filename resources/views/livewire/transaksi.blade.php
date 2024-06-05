1246a8f89406baafb910aa8421c55ed510b2fe10
[BangAlek29] <[dzikribassyrilm@gmail.com]>
2024-06-05 09:39:38 +0700

    push

--- resources/views/livewire/transaksi.blade.php
+++ resources/views/livewire/transaksi.blade.php
@@ -1,95 +1,33 @@
-<div class="w-full h-full ">
-    <div class="w-full h-[1388px] top-[180px] absolute bg-zinc-300"></div>
+<div class="bg-pink-100">
 
-    <div class="absolute top-0 left-0 w-full max-w-screen-xl mx-auto px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
-        <!-- Transaction Information -->
-        <div class="text-black text-2xl font-['Inria Serif'] mb-4">
-            <p>No Transaksi: E-202487</p>
-            <p>Tanggal Transaksi: 24 Mei 2024</p>
-            <p>Tanggal Acara: 24 Mei 2024</p>
-            <p>Nama Pengantin Pria: David</p>
-            <p>Nama Pengantin Wanita: Putri</p>
-        </div>
-
-        <!-- Purchase List -->
-        <div class="border border-black w-full mb-8">
-            <div class="grid grid-cols-4 text-black text-xl font-['Inter'] font-medium">
-                <div class="col-span-1 border-r border-black p-2">
-                    <p>List Pembelian</p>
-                </div>
-                <div class="col-span-1 border-r border-black p-2">
-                    <p>Keterangan</p>
-                </div>
-                <div class="col-span-1 border-r border-black p-2">
-                    <p>Kontak</p>
-                </div>
-                <div class="col-span-1 p-2">
-                    <p>Harga</p>
-                </div>
-            </div>
-            <div class="grid grid-cols-4 text-black text-xl font-['Inter'] font-medium border-t border-black">
-                <div class="col-span-1 border-r border-black p-2">
-                    <p>Nama Vendor</p>
-                    <p>Nama Mobil</p>
-                    <p>Tim Perias</p>
-                    <p>Tim Akom dan Decor</p>
-                    <p>Catering</p>
-                    <p>Souvenir</p>
-                    <p>Entertainment</p>
-                    <p>MC</p>
-                    <p>Nama Venue</p>
-                    <p>Dokumentasi</p>
-                </div>
-                <div class="col-span-1 border-r border-black p-2">
-                    <p>Nama Vendor</p>
-                    <p>Nama Mobil</p>
-                    <p>Tim Perias</p>
-                    <p>Tim Akom dan Decor</p>
-                    <p>Catering</p>
-                    <p>Souvenir</p>
-                    <p>Entertainment</p>
-                    <p>MC</p>
-                    <p>Nama Venue</p>
-                    <p>Dokumentasi</p>
-                </div>
-                <div class="col-span-1 border-r border-black p-2">
-                    <p>081264819867</p>
-                    <p>081264819867</p>
-                    <p>081264819867</p>
-                    <p>081264819867</p>
-                    <p>081264819867</p>
-                    <p>081264819867</p>
-                    <p>081264819867</p>
-                    <p>081264819867</p>
-                    <p>081264819867</p>
-                    <p>081264819867</p>
-                </div>
-                <div class="col-span-1 p-2">
-                    <p>Rp. 1.000.000,00</p>
-                    <p>Rp. 500.000,00</p>
-                    <p>Rp. 2.000.000,00</p>
-                    <p>Rp. 1.500.000,00</p>
-                    <p>Rp. 500.000,00</p>
-                    <p>Rp. 900.000,00</p>
-                    <p>Rp. 100.000.000,00</p>
-                    <p>Rp. 20.000.000,00</p>
-                    <p>Rp. 40.000.000,00</p>
-                    <p>Rp. 10.000.000,00</p>
-                </div>
-            </div>
-            <div class="grid grid-cols-4 text-black text-xl font-['Inter'] font-medium border-t border-black">
-                <div class="col-span-3 border-r border-black p-2 text-right">
-                    <p>Sub Total</p>
-                </div>
-                <div class="col-span-1 p-2">
-                    <p>Rp. 150.600.000,00</p>
-                </div>
+    <!-- isi transaksi -->
+    <div class="card-container">
+    <article class="max-w-full mx-auto overflow-hidden rounded-lg shadow transition hover:shadow-lg mt-4 bg-white">
+        <div class="flex">
+            <img    
+                alt=""
+                src="https://images.unsplash.com/photo-1524758631624-e2822e304c36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+                class="h-56 object-cover"
+            />
+            <div class="flex-grow p-4 sm:p-6">
+                <time datetime="2022-10-10" class="block text-xs text-gray-500"> 10th Oct 2022 </time>
+                <p class="mt-2 line-clamp-3 text-sm/relaxed text-gray-500">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae dolores, possimus
+                    pariatur animi temporibus nesciunt praesentium dolore sed nulla ipsum eveniet corporis quidem,
+                    mollitia itaque minus soluta, voluptates neque explicabo tempora nisi culpa eius atque
+                    dignissimos. Molestias explicabo corporis voluptatem?
+                </p>
             </div>
         </div>
-
-        <!-- Back Button -->
-        <div class="w-full flex justify-end">
-            <a href="#" class="w-[337px] h-[105px] bg-black text-white text-2xl font-['Inria Serif'] rounded-[20px] flex items-center justify-center mix-blend-overlay">Back</a>
+        <div class="flex justify-end p-4 sm:p-6">
+            <a href="#"
+                class="inline-block mt-2 rounded bg-indigo-600 px-4 py-2 text-lg font-medium
+                text-white transition hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-yellow-400">
+                PESANAN MU
+            </a>
         </div>
-    </div>
+    </article>
+</div>
+
+    <!-- end transaksi -->
 </div>
